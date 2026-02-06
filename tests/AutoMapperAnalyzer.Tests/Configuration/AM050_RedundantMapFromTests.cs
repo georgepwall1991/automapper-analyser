6f7b0fb9f5a85d0c9860b75f748ed47135bd31e9
George Wall <84231788+georgepwall1991@users.noreply.github.com>
2026-02-06 16:13:29 +0000

    Improve AM050 redundancy detection false-positive handling (#50)

--- tests/AutoMapperAnalyzer.Tests/Configuration/AM050_RedundantMapFromTests.cs
+++ tests/AutoMapperAnalyzer.Tests/Configuration/AM050_RedundantMapFromTests.cs
@@ -75,4 +75,76 @@ public class AM050_RedundantMapFromTests
 
         await AnalyzerVerifier<AM050_RedundantMapFromAnalyzer>.VerifyAnalyzerAsync(testCode);
     }
+
+    [Fact]
+    public async Task Should_NotReportDiagnostic_When_MappingFromCapturedVariable()
+    {
+        const string testCode = """
+                                using AutoMapper;
+
+                                public class Source { public string Name { get; set; } }
+                                public class Destination { public string Name { get; set; } }
+
+                                public class MyProfile : Profile
+                                {
+                                    public MyProfile()
+                                    {
+                                        var other = new Source { Name = "captured" };
+                                        CreateMap<Source, Destination>()
+                                            .ForMember(d => d.Name, o => o.MapFrom(s => other.Name));
+                                    }
+                                }
+                                """;
+
+        await AnalyzerVerifier<AM050_RedundantMapFromAnalyzer>.VerifyAnalyzerAsync(testCode);
+    }
+
+    [Fact]
+    public async Task Should_NotReportDiagnostic_ForNonAutoMapperForMemberAndMapFrom()
+    {
+        const string testCode = """
+                                using System;
+
+                                public class Source { public string Name { get; set; } }
+                                public class Destination { public string Name { get; set; } }
+
+                                public class FakeOptions<TSource, TDestination>
+                                {
+                                    public FakeOptions<TSource, TDestination> MapFrom(Func<TSource, object> map)
+                                    {
+                                        return this;
+                                    }
+                                }
+
+                                public class FakeBuilder<TSource, TDestination>
+                                {
+                                    public FakeBuilder<TSource, TDestination> ForMember(
+                                        Func<TDestination, object> destination,
+                                        Func<FakeOptions<TSource, TDestination>, FakeOptions<TSource, TDestination>> config)
+                                    {
+                                        return this;
+                                    }
+                                }
+
+                                public class FakeProfile
+                                {
+                                    public FakeBuilder<TSource, TDestination> CreateMap<TSource, TDestination>()
+                                    {
+                                        return new FakeBuilder<TSource, TDestination>();
+                                    }
+                                }
+
+                                public class Consumer
+                                {
+                                    public void Configure()
+                                    {
+                                        new FakeProfile()
+                                            .CreateMap<Source, Destination>()
+                                            .ForMember(d => d.Name, o => o.MapFrom(s => s.Name));
+                                    }
+                                }
+                                """;
+
+        await AnalyzerVerifier<AM050_RedundantMapFromAnalyzer>.VerifyAnalyzerAsync(testCode);
+    }
 }
